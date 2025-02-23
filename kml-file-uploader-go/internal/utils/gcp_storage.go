0965c6c19db3445d38800fbc10cb3e8b08a14b91
Guilherme Sampaio <guilhermefs09@protonmail.com>
2025-02-23 16:58:53 -0300

    update: GCP create client function

--- kml-file-uploader-go/internal/utils/gcp_storage.go
+++ kml-file-uploader-go/internal/utils/gcp_storage.go
@@ -8,38 +8,50 @@ import (
 	"os"
 
 	"cloud.google.com/go/storage"
+	"google.golang.org/api/option"
 )
 
 type GCPClient struct {
 	bucketName string
 }
 
-func (gcp *GCPClient) createClient(ctx context.Context) (*storage.Client, error) {
-	client, err := storage.NewClient(ctx)
+func (gcp *GCPClient) createClient(ctx context.Context, serviceAccCreds string) (*storage.Client, error) {
+	var client *storage.Client
+	var err error
+
+	if serviceAccCreds != "" {
+		client, err = storage.NewClient(ctx, option.WithCredentialsFile(serviceAccCreds))
+	} else {
+		client, err = storage.NewClient(ctx)
+	}
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to create client: %w", err)
 	}
 	return client, nil
 }
 
+
 func (gcp *GCPClient) SetBucket() error {
 	bucketName := os.Getenv("bucket_name")
 	if bucketName == "" {
-		return errors.New("Variable bucket_name is not defined")
+		return errors.New("bucket_name is not set in environment variables")
 	}
 
 	gcp.bucketName = bucketName
 	return nil
 }
 
-func (gcp *GCPClient) Upload(file *os.File, filePath string, objectName string) error {
-	ctx := context.Background()
 
-	client, err := gcp.createClient(ctx)
+func (gcp *GCPClient) Upload(file *os.File, filePath string, objectName string) error {
+	serviceAccCreds := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	ctx := context.Background()
+	client, err := gcp.createClient(ctx, serviceAccCreds)
 	if err != nil {
 		return err
 	}
 	defer client.Close()
+
 	bucket := client.Bucket(gcp.bucketName)
 
 	defer file.Close()
