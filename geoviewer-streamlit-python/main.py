4da1068728324a62aad8496dbb5ff6ebde0d79ba
Guilherme Sampaio <guilhermefs09@protonmail.com>
2025-03-22 20:24:45 -0300

    update: layout

--- geoviewer-streamlit-python/main.py
+++ geoviewer-streamlit-python/main.py
@@ -5,35 +5,37 @@ if "logged_in" not in st.session_state:
 if "tokenid" not in st.session_state:
     st.session_state["tokenid"] = ""
 
+
 def logout():
     st.session_state["tokenid"] = ""
     st.session_state.logged_in = False
     st.rerun()
 
+
 logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 login_page = st.Page("views/auth.py", title="Log out", icon=":material/logout:")
 
-dashboard = st.Page(
-    "views/map.py", title="Dashboard", icon=":material/map:", default=True
+map_dashboard = st.Page(
+    "views/map.py", title="Map", icon=":material/map:", default=False
 )
 
-dashboard = st.Page(
-    "views/upload.py", title="Upload", icon=":material/map:", default=True
+upload_dashboard = st.Page(
+    "views/upload.py", title="KML", icon=":material/folder:", default=True
 )
 
 if st.session_state.logged_in:
     pg = st.navigation(
         {
-            "Account": [logout_page],
-            "Maps": [dashboard],
-            "Tools": [],
+            "Account 👤": [logout_page],
+            "Upload 📑": [upload_dashboard],
+            "Dashboard 🌍": [map_dashboard],
         }
     )
 else:
-    pg = st.navigation({
+    pg = st.navigation(
+        {
             "Account": [login_page],
-            "Maps": [],
-            "Tools": [],
+            "Map": [],
         }
     )
 
